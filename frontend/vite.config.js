b16cb8e773dd06f8576047ea7745a849011f6234
eleftheriosvarv <varverakis.e@live.unic.ac.cy>
2025-04-04 01:06:28 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    allowedHosts: ["frontend-dashboard-fyjc.onrender.com"]
-  },
-  preview: {
-    host: true,
-    port: 4173
+    host: true
   }
 })
+
