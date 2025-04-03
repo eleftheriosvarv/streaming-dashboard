ae51480e0dc93f781a58dc3219fa1d9c6ac8fa8d
eleftheriosvarv <varverakis.e@live.unic.ac.cy>
2025-04-04 00:42:17 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
+// frontend/src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Dashboard from './Dashboard.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Dashboard />
   </React.StrictMode>
 )
-
 
