f0abd2aee0f5fbb56ef659cfc7d426a7ec62b821
eleftheriosvarv <varverakis.e@live.unic.ac.cy>
2025-04-19 19:16:08 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Dashboard from './Dashboard.jsx';
-import './index.css'; // 👈 ΠΡΟΣΘΗΚΗ για Tailwind!
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
